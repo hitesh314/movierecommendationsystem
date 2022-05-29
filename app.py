025022e0e60bbc2e97c881f53580bf6fbc3954af
hitesh314 <84220808+hitesh314@users.noreply.github.com>
2022-05-29 13:18:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pickle
 import streamlit as st
 import requests
 import pandas as pd
+import bz2
+import _pickle as cPickle
 st.set_page_config(layout="wide")
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
@@ -15,7 +17,10 @@ def fetch_poster(movie_id):
     poster_path = data['poster_path']
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
-
+def decompress_pickle(file):
+    data = bz2.BZ2File(file, "rb")
+    data = cPickle.load(data)
+    return data
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
@@ -28,10 +33,10 @@ def recommend(movie):
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
     return recommended_movie_names, recommended_movie_posters
-
+data = decompress_pickle('similarity.pbz2')
 movies_dict=pickle.load(open('movie_dict.pkl','rb'))
 movies=pd.DataFrame(movies_dict)
-similarity=pickle.load(open('similarity.pkl','rb'))
+similarity=data
 st.title('Movie Recommender System')
 selected_movie=st.selectbox('Name a Movie',movies['title'].values)
 
